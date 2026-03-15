1c9e66e60771d5667459f6c70563eba1ab5ee8e1
Your Name <you@example.com>
2026-03-15 07:26:34 +0530

    serv

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import { FaRegBookmark, FaBookmark } from "react-icons/fa";
 function App() {
   const [news, setNews] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [favorites, setFavorites] = useState(() => {
+    const saved = localStorage.getItem("favorites");
+    return saved ? JSON.parse(saved) : [];
+  });
   useEffect(() => {
     const fetchNews = async () => {
       setLoading(true);
@@ -13,28 +17,53 @@ function App() {
           `${import.meta.env.VITE_API_URI}/api/hindu`,
         );
         console.log("res: ", res?.data);
-        setNews(res?.data || []);
+        setNews(res?.data?.items || []);
         setLoading(false);
       } catch (error) {
         console.log("error: ", error?.response);
       } finally {
-        setLoading(true);
+        setLoading(false);
       }
     };
     fetchNews();
   }, []);
   if (loading) {
-    <div className="grid place-items-center h-screen w-full bg-white">
-      <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
-    </div>;
+    return (
+      <div className="grid place-items-center h-screen w-full bg-white">
+        <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+      </div>
+    );
   }
-  const toggleBookmark = async (n) => {
-    try {
-      await axios.post(`${import.meta.env.VITE_API_URI}/api/favorite`, n);
-    } catch (err) {
-      console.log(err);
-    }
+  const toggleBookmark = (link) => {
+    setFavorites((prev) => {
+      let updated;
+
+      if (prev.includes(link)) {
+        updated = prev.filter((l) => l !== link);
+      } else {
+        updated = [...prev, link];
+      }
+
+      localStorage.setItem("favorites", JSON.stringify(updated));
+      return updated;
+    });
   };
+  // const toggleBookmark = async (n) => {
+  //   try {
+  //     const res = await axios.post(
+  //       `${import.meta.env.VITE_API_URI}/api/favorite`,
+  //       n,
+  //     );
+  //     console.log("tick: ", res?.data);
+  //     setNews((prev) =>
+  //       prev.map((item) =>
+  //         item.link === n.link ? { ...item, favorite: !item.favorite } : item,
+  //       ),
+  //     );
+  //   } catch (err) {
+  //     console.log("tick error: ", err?.response);
+  //   }
+  // };
   return (
     <div className="min-h-screen bg-gray-100 p-6">
       <h1 className="text-2xl font-bold mb-2 text-center text-black">
@@ -42,14 +71,27 @@ function App() {
       </h1>
       <p className="mb-6 text-center text-black">Kanha Gupta</p>
       <div className="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-        {news?.items?.map((n, idx) => (
+        {news?.map((n, idx) => (
           <div
             key={idx}
             className="bg-white rounded-xl shadow-md p-5 hover:shadow-xl transition duration-300 flex flex-col justify-between"
           >
-            <div className="text-xs text-blue-500 font-semibold mb-2">
-              {n?.tags?.length ? n.tags.join(", ") : "No Tags"}
+            <div className="flex justify-between">
+              <div className="text-xs text-blue-500 font-semibold mb-2">
+                {n?.tags?.length ? n.tags.join(", ") : "No Tags"}
+              </div>
+              <button
+                onClick={() => toggleBookmark(n?.link)}
+                className="w-fit p-1 rounded-full  transition bg-transparent border-none"
+              >
+                {favorites.includes(n?.link) ? (
+                  <FaBookmark className="text-red-500 text-xl" />
+                ) : (
+                  <FaRegBookmark className="text-gray-400 text-xl hover:text-red-500 transition" />
+                )}
+              </button>
             </div>
+
             <div className="text-xs text-gray-500/50 font-semibold mb-2">
               {n?.pubDate}
             </div>
@@ -61,13 +103,7 @@ function App() {
             <p className="text-sm text-gray-600 mb-4 line-clamp-3">
               {n?.description}
             </p>
-            <button onClick={() => toggleBookmark({link:n?.link})} className="w-fit p-0 flex items-center justify-center">
-              {n.favorite ? (
-                <FaBookmark className="text-blue-600 text-lg" />
-              ) : (
-                <FaRegBookmark className="text-gray-400 text-lg" />
-              )}
-            </button>
+
             <a
               href={n?.link}
               target="_blank"
